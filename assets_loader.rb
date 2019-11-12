11d9235a01d0694b5ee3a2b322d8b6985cc41ba0
Jonathan <48403308+mesielepush@users.noreply.github.com>
2019-11-12 14:23:05 -0600

    Movements

--- assets_loader.rb
+++ assets_loader.rb
@@ -1,12 +1,37 @@
 def load_audiovisual
     img = {
-        background: Gosu::Image.new("images/cover.png"),
-        
+        cover: Gosu::Image.new("images/cover.png"),
+        off: Gosu::Image.new('images/off.png'),
+        background: Gosu::Image.new('images/back.png'),
+        to_enter: Gosu::Image.new('images/enter_for_enter.png'),
+        off_pigeon: Gosu::Image.new('images/off_p.png'),
+        pigeon_init: Gosu::Image.new('images/PINI.png'),
+        walk_right: Gosu::Image.new('images/PDER.png'),
+        walk_left: Gosu::Image.new('images/PIZQ.png'),
+        rigth_click: Gosu::Image.new('images/P2Pellet.png'),
+        left_click: Gosu::Image.new('images/P1Pellet.png'),
+
+
     }
     sounds = {
         #intro: Gosu::Song.new('intro.mp3'),
-        enter: Gosu::Sample.new('sounds/game.mp3'),
-        masking: Gosu::Song.new('sounds/masking_sound.mp3')
+        enter: Gosu::Sample.new('sounds/coin.mp3'),
+        masking: Gosu::Song.new('sounds/masking_sound.mp3'),
+        on: Gosu::Sample.new('sounds/on.mp3'),
+        click: Gosu::Sample.new('sounds/click.mp3')
     }
     return img, sounds
+end
+def load_metadata
+    key =  {kb_left: Gosu::KbLeft,
+            kb_right: Gosu::KbRight,
+            gp_left: Gosu::GpLeft,
+            gp_right: Gosu::GpRight,
+            enter: Gosu::KB_RETURN,
+            space: Gosu::KB_SPACE,
+            m_left: Gosu::MS_LEFT,
+            m_right: Gosu::MS_RIGHT
+            }
+    
+    return key
 end
--- init.rb
+++ init.rb
@@ -7,23 +7,106 @@ class Game < Gosu::Window
         super 800, 600
         self.caption = 'Poscastenabler'
 
-        #@keyboard, @schedules = load_metadata
+        @keyboard = load_metadata
         @img, @sounds = load_audiovisual
-        @sounds[:enter].play(volume = 0.1)
+        @sounds[:enter].play(volume = 0.3)
+        @sounds[:on].play(volume = 0.3)
+        @sounds[:masking].volume = 0.8
+        @sounds[:masking].play(true)
+        
+        @y_pigeon_intro = 700
+        @x_pigeon = 320
+        @y_pigeon = 280
         @intro = true 
+        @initial_scene = false
+        @Session = false
     end
 
     
 
     def update
-       
+
+       if @initial_scene == true
+            @y_pigeon_intro -= 5
+            if @y_pigeon_intro < 280
+                @y_pigeon_intro = 280
+            end
+        end
+        if @Session == true
+            if @x_pigeon < 40
+                
+                @x_pigeon = 40
+            
+            elsif @x_pigeon > 500
+                @x_pigeon = 500
+            end
+
+            if Gosu::button_down? @keyboard[:kb_left]
+                @x_pigeon -= 5
+                
+            elsif Gosu::button_down? @keyboard[:kb_right]
+                @x_pigeon +=5
+                
+            end
+
+            if @x_pigeon == 40 and Gosu::button_down? @keyboard[:space]
+                @sounds[:click].play(volume=0.5)
+                       
+                
+            end
+        end
+
     end
     def draw
         if @intro == true
+            @img[:cover].draw(0, 0, 0)
+            if Gosu::button_down? @keyboard[:space]
+                @sounds[:enter].play
+                @sounds[:on].play(volume = 1, speed = 1, looping = false)
+                @initial_scene = true
+                @intro = nil
+            end
+        end
+        
+        if @initial_scene == true
+            @img[:off].draw(0, 0, 0)
+            @img[:off_pigeon].draw( 350, @y_pigeon_intro, 0)
+            
+            if @y_pigeon_intro > 198
+                if Gosu::button_down? @keyboard[:kb_left] || @keyboard[:kb_rigth]
+                    @Session = true
+                    @initial_scene = nil
+                end
+            end
+        end
+
+        if @Session == true
+            puts @x_pigeon
             
             @img[:background].draw(0, 0, 0)
-        end
-        if @Session == false
+            
+            if Gosu::button_down? @keyboard[:kb_left]
+                @img[:walk_left].draw(@x_pigeon, @y_pigeon, 0)
+            elsif Gosu::button_down? @keyboard[:kb_right]
+                @img[:walk_right].draw(@x_pigeon, @y_pigeon, 0)
+            elsif @x_pigeon == 500
+                if Gosu::button_down? @keyboard[:space]
+                    @img[:rigth_click].draw(@x_pigeon-40, @y_pigeon-40, 0)
+                    @sounds[:click].play(volume = 0.9, speed = 1, looping = false)
+                else
+                    @img[:pigeon_init].draw(@x_pigeon, @y_pigeon, 0)
+                end
+            elsif @x_pigeon == 40
+                if Gosu::button_down? @keyboard[:space]
+                    @img[:left_click].draw(@x_pigeon-30, @y_pigeon-40, 0)
+                    
+                else
+                    @img[:pigeon_init].draw(@x_pigeon, @y_pigeon, 0)
+                end    
+            else
+                @img[:pigeon_init].draw(@x_pigeon, @y_pigeon, 0)
+            end
+            
         end
     end
 end
